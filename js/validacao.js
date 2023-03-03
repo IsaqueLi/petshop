809d666e5f3bfb7f1c5d0c98312284045be4a095
IsaqueLi <isakilica16@gmail.com>
2023-03-03 16:29:08 -0300

    implementando calculo de digitos para validacao do cpf

--- js/validacao.js
+++ js/validacao.js
@@ -80,7 +80,7 @@ function validaCPF(input){
     const cpfFormatado = input.value.replace(/\D/g, '')
     let mensagem = '';
 
-    if(!checaCPFRepetido(cpfFormatado)){
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
         mensagem = 'O CPF digitado não é válido.';
     }
 
@@ -109,4 +109,36 @@ function checaCPFRepetido(cpf){
     })
 
     return cpfValido
+}
+
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10;
+
+    return checaDigitoVerificador(cpf, multiplicador) 
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+    
+    if(multiplicador >= 12){
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador;
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoverificador = cpf.charAt(multiplicador - 1);
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+        contador++;
+    }
+
+    if(digitoverificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1);
+    }
+
+    return false
+}
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11);
 }
